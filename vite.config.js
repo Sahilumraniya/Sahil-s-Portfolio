62bda1e3579426e8f08cb250bfae92ac9e950f7b
sahilumraniya <sahilumraniya9512@gmail.com>
2024-07-13 20:08:13 +0530

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+    },
+    server: {
+        open: true,
+    },
+    preview: {
+        port: 4173
+    }
 })
